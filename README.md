4bf6089b4fea5a6b94f511127a3375a47de88c3d
MFG38 <santtu.pesonen@mail.com>
2022-10-22 21:24:36 +0300

    it exists woo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mint-debloat
-A shell script for debloating a Linux Mint install.
+A shell script for debloating a Linux Mint install. Doesn't really do much yet, just uninstalls some packages that might prove useless to the average user.
+
+**NOTE:** This script was designed on top of Linux Mint 20.2. It may or may not work properly on other versions of the OS.
---
+++ debloat.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo This script will remove these packages:
+echo -- celluloid
+echo -- ed
+echo -- hexchat
+echo -- hexchat-common
+echo -- hypnotix
+echo -- redshift
+echo -- thunderbird
+echo -- youtube-dl
+echo Double-check that you do not need any of the above packages
+echo before proceeding.
+echo 
+
+read -p "Y+Enter to proceed or N+Enter to cancel." CHOICE
+
+if [ ${CHOICE} == y ]
+then
+    sudo apt remove celluloid ed hexchat hexchat-common hypnotix redshift thunderbird youtube-dl
+    sudo apt purge celluloid ed hexchat hexchat-common hypnotix redshift thunderbird youtube-dl
+elif [ ${CHOICE} == n ]
+then
+    echo Debloating cancelled.
+fi
